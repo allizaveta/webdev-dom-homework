1142b08a418aa97848914d337adf08611995d093
Allizaveta <allizaveta@gmail.com>
2024-04-11 09:00:28 +0300

    neW MODULE "send"

---
+++ answer.js
@@ -0,0 +1,12 @@
+const initAnswerListeners = () => {
+    const headElements = document.querySelectorAll(".comment-header");
+    headElements.forEach((headElement, index) => {
+        headElement.addEventListener('click', () => {
+            const commentText = comments[index].text;
+            const commentAuthor = comments[index].name;
+            textInputElement.value = `>${commentText}\n${commentAuthor}`;
+        });
+    });
+};
+
+initAnswerListeners();
--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 import { catchErrorPost, catchErrorGet } from "./errorHandler.js";
+import { send } from "./send.js";
 
 const nameInputElement = document.getElementById("name-input");
 const textInputElement = document.getElementById("text-input");
 const buttonElement = document.getElementById("send-button");
 const preloader = document.getElementById("preloader");
-export const comments = [];
+const comments = [];
 
 catchErrorGet(comments);
-
-buttonElement.addEventListener('click', () => {
-    nameInputElement.classList.remove('error');
-    textInputElement.classList.remove('error');
-
-    if (nameInputElement.value.trim() === '' || textInputElement.value.trim() === '') {
-        nameInputElement.classList.add('error');
-        textInputElement.classList.add('error');
-        return;
-    }
-
-    preloader.classList.remove('preloader-hidden');
-    catchErrorPost(nameInputElement, textInputElement, comments);
-    catchErrorGet(comments);
-});
+send(buttonElement, textInputElement, nameInputElement, comments, preloader);
