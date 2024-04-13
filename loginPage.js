584544e210488c0efc8414a924a8d4976baeac63
Allizaveta <allizaveta@gmail.com>
2024-04-13 22:58:43 +0300

    исрпавила ошибку

--- loginPage.js
+++ loginPage.js
@@ -1,36 +1,36 @@
-import { login,setToken } from "./api.js";
+import { login, setToken } from "./api.js";
 import { catchErrorGet } from "./errorHandler.js";
+import { renderComments } from "./renderComments.js";
 
-export const renderLogin = () =>{
-    const appElement = document.getElementById("app")
+export const renderLogin = () => {
+    const appElement = document.getElementById("app");
     const loginHtml = `<div class="form">
-    <h3>Форма входа</h3>
-    <div class="form--row">
-    <input type="text"
-    id="login-input"
-    class="input"
-    placeholder="Логин">
-    <input type="password"
-    id="password-input"
-    class="input"
-    placeholder="Пароль">
-    </div>
-    <br>
-    <button class="button" id="login-button">Войти</button>
+        <h3>Форма входа</h3>
+        <div class="form--row">
+            <input type="text" id="login-input" class="input" placeholder="Логин">
+            <input type="password" id="password-input" class="input" placeholder="Пароль">
+        </div>
+        <br>
+        <button class="button" id="login-button">Войти</button>
     </div>`;
     appElement.innerHTML = loginHtml;
 
     const buttonElement = document.getElementById("login-button");
     const loginInputElement = document.getElementById("login-input");
-    const passwordInputElement = document.getElementById("login-input");
-    
-        buttonElement.addEventListener("click", () => {
-            login({login:loginInputElement.value,
-                password: passwordInputElement.value,
-            }).then((responseData)=>{
-                setToken(responseData.user.token);
-            }).then(()=>{
-                catchErrorGet();
-            })
+    const passwordInputElement = document.getElementById("password-input");
+
+    buttonElement.addEventListener("click", () => {
+        login({
+            login: loginInputElement.value,
+            password: passwordInputElement.value,
+        }).then((responseData) => {
+            setToken(responseData.user.token);
+            // Обновляем элемент с классом .add-form-name только после успешной авторизации
+            const loggedInUserName = responseData.user.name;
+            const nameInputElement = document.querySelector(".add-form-name");
+            
+            catchErrorGet();
         });
+    });
 };
+
